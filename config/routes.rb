eb2b36a740c5a5bfa2151817eeebb519dbccadcc
Yuta Yamaki <8makicam@gmail.com>
2021-03-06 01:54:50 +0000

    [add]homesコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'homes#top'
+  get "/about" => "homes#about"
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
